369a3c926f29688c16748c03137ccca2d2fc440a
P3ntest <p3ntest@gmx.de>
2022-06-22 17:11:18 +0200

    add music

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,38 @@
 require("dotenv").config();
 
 const { Client } = require("discord.js");
-const { joinVoiceChannel, getVoiceConnection } = require("@discordjs/voice");
+const {
+  joinVoiceChannel,
+  getVoiceConnection,
+  createAudioPlayer,
+  AudioPlayerStatus,
+  createAudioResource,
+  StreamType,
+} = require("@discordjs/voice");
+const path = require("path");
 
 const client = new Client({
   intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_VOICE_STATES"],
   partials: ["CHANNEL"],
+});
+
+const player = createAudioPlayer();
+
+let resource;
+
+resource = createAudioResource(
+  path.join(__dirname, "../assets/audio/music.ogg"),
+  {
+    inputType: StreamType.OggOpus,
+    inlineVolume: true,
+  }
+);
+
+resource.volume.setVolume(0.005);
+
+player.play(resource);
+player.on(AudioPlayerStatus.Idle, () => {
+  player.play(resource);
 });
 
 const guildConnections = {};
@@ -14,15 +41,19 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
   const channel = newState.channel ?? oldState.channel;
   const members = channel.members.filter((member) => !member.user.bot).size;
 
-  if (members == 2) {
+  if (members == 3) {
+    const con = joinVoiceChannel({
+      channelId: channel.id,
+      guildId: channel.guild.id,
+      adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+
     guildConnections[channel.guild.id] = {
       channelId: channel.id,
-      connection: joinVoiceChannel({
-        channelId: channel.id,
-        guildId: channel.guild.id,
-        adapterCreator: channel.guild.voiceAdapterCreator,
-      }),
+      connection: con,
     };
+
+    con.subscribe(player);
   } else {
     if (guildConnections[channel.guild.id]?.channelId == channel.id) {
       getVoiceConnection(channel.guild.id)?.destroy();
